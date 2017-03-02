c7fca0ad00a138b4ec94383a5c99a1451a4be0da
Predix Robot <predix-robot@ge.com>
2017-03-02 12:14:39 -0800

    Release 1.1.9

--- scripts/generate_machine_container.sh
+++ scripts/generate_machine_container.sh
@@ -1,36 +1,48 @@
 #!/bin/bash
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="`pwd`"
 #######Functions
 ##################### Variables Section End   #####################
+function getRepoURL {
+	local  repoURLVar=$2
+	reponame=$(echo "$1" | awk -F "/" '{print $NF}')
+	url=$(echo "$a" | sed -n "/$reponame/p" $CURRENT_DIR/version.json | awk -F"\"" '{print $4}' | awk -F"#" '{print $1}')
+	eval $repoURLVar="'$url'"
+}
+function getRepoVersion {
+	local  repoVersionVar=$2
+	reponame=$(echo "$1" | awk -F "/" '{print $NF}')
+	repo_version="$(echo "$a" | sed -n "/$reponame/p" $CURRENT_DIR/version.json | awk -F"\"" '{print $4}' | awk -F"#" '{print $NF}')"
+	eval $repoVersionVar="'$repo_version'"
+}
 __echo_run() {
-echo $@
-$@
-return $?
+  echo $@
+  $@
+  return $?
 }
+
 __print_center() {
-len=${#1}
-sep=$2
-buf=$((($COLUMNS-$len-2)/2))
-line=""
-for (( i=0; i < $buf; i++ )) {
-line="$line$sep"
-}
-line="$line $1 "
-for (( i=0; i < $buf; i++ )) {
-line="$line$sep"
-}
-echo ""
-echo $line
+  len=${#1}
+  sep=$2
+  buf=$((($COLUMNS-$len-2)/2))
+  line=""
+  for (( i=0; i < $buf; i++ )) {
+  line="$line$sep"
+  }
+  line="$line $1 "
+  for (( i=0; i < $buf; i++ )) {
+    line="$line$sep"
+  }
+  echo ""
+  echo $line
 }
 #######Functions end##########
 arguments="$*"
-if [ -d "predix-scripts" ]; then
- cd predix-scripts
- git pull
- cd ..
-else
- __echo_run git clone https://github.com/PredixDev/predix-scripts.git -b develop
-fi
+rm -rf predix-scripts
+getRepoURL "predix-scripts" predix_scripts_url
+getRepoVersion "predix-scripts" predix_scripts_version
+echo "predix_scripts_url : $predix_scripts_url"
+echo "predix_scripts_version : $predix_scripts_version"
+__echo_run git clone "$predix_scripts_url" -b $predix_scripts_version
 
 __print_center "Creating Machine Container" "#"
 
@@ -44,5 +56,8 @@ cd predix-scripts/bash
 
 ./quickstart.sh -cm $arguments
 
-cd $CURRENT_DIR/..
+echo "MACHINE_CONTAINER_TYPE : $MACHINE_CONTAINER_TYPE"
+echo "MACHINE_VERSION : $MACHINE_VERSION"
+cp $CURRENT_DIR/predix-scripts/bash/PredixMachine$MACHINE_CONTAINER_TYPE-$MACHINE_VERSION.zip $CURRENT_DIR
+
 pwd
